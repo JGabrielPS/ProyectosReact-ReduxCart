db313b1d5efb59c92b5088be2421ec7dc92d9d39
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-24 19:07:19 -0300

    new: add redux createStore and Provider. Also reducer and increase and decrease actions. Delete cart property in CartContainer because implementation of Provider and store

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducer";
+import { INCREASE, DECREASE } from "./actions";
+
+const initialStore = {
+  cart: cartItems,
+  total: 1000,
+  amount: 1,
+};
+
+const store = createStore(reducer, initialStore);
 
 function App() {
   // cart setup
 
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
+      <CartContainer />
+    </Provider>
   );
 }
 
