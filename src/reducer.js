cb26fd6b935c748ed85ddd9f0d0028f530d13ee2
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-25 17:10:24 -0300

    new: remove inneccesary actions (increase, decrease) and rename others (get totals and toggle amount). Add initial state for store with its items file imported and delete increase and decrease actions (replaced with toggle amount) and add get totals

--- src/reducer.js
+++ src/reducer.js
@@ -1,28 +1,58 @@
-import {
-  INCREASE,
-  DECREASE,
-  REMOVE,
-  GET_TOTAL,
-  GET_AMOUNT,
-  CLEAR_CART,
-} from "./actions";
+import { REMOVE, GET_TOTALS, TOGGLE_AMOUNT, CLEAR_CART } from "./actions";
+import cartItems from "./cart-items";
 
-const reducer = (state, action) => {
+const initialStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+
+const reducer = (state = initialStore, action) => {
   if (action.type === CLEAR_CART) {
     return { ...state, cart: [] };
   }
 
-  if (action.type === DECREASE) {
-  }
-
-  if (action.type === INCREASE) {
+  if (action.type === TOGGLE_AMOUNT) {
+    return {
+      ...state,
+      cart: state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.toggle === "inc") {
+            cartItem = { ...cartItem, amount: cartItem.amount + 1 };
+          } else if (action.payload.toggle === "dec") {
+            cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+          }
+        }
+        return cartItem;
+      }),
+    };
   }
 
   if (action.type === REMOVE) {
     return {
       ...state,
-      cart: state.cart.filter((item) => item.id !== action.payload.id),
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload.id),
     };
+  }
+
+  if (action.type === GET_TOTALS) {
+    let { amount, total } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+        cartTotal.amount += amount;
+        cartTotal.total += itemTotal;
+        return cartTotal;
+      },
+      {
+        amount: 0,
+        total: 0,
+      }
+    );
+
+    total = parseFloat(total.toFixed(2));
+
+    return { ...state, amount, total };
   }
 
   return state;
