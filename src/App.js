d999f5a3fc0e2db2d72f16f9419af2f1f7d40cdd
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-25 17:07:04 -0300

    new: remove actions, cart items and initial store (to reduce file). In createStore added code for redux dev tools

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import React from "react";
 // components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
-// items
-import cartItems from "./cart-items";
+
 // redux stuff
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducer";
-import { INCREASE, DECREASE } from "./actions";
 
-const initialStore = {
-  cart: cartItems,
-  total: 1000,
-  amount: 1,
-};
-
-const store = createStore(reducer, initialStore);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 function App() {
   // cart setup
